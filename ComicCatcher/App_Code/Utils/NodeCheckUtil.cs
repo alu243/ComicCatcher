c07adbe81acf06283666160aefc1aec0a3e104a9
alu243 <alu243@gmail.com>
2014-09-26 00:36:03 +0800

    addFile

---
+++ ComicCatcher/App_Code/Utils/NodeCheckUtil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utils
+{
+    /// <summary>
+    /// 判斷節點類別 Web -> List(page) -> ComicName -> Chapter
+    /// </summary>
+    class NodeCheckUtil
+    {
+        /// <summary>
+        /// 是否為顯示回數(集數)的節點
+        /// </summary>
+        /// <param name="tn"></param>
+        /// <returns></returns>
+        public static bool IsChapterNode(TreeNode tn)
+        {
+            return tn.Level == 3;
+            //if (null == tn) return false;
+            //if (false == tn.Name.Contains(XindmWebSite.WebUrl)) return false; // 節點本身必需要是包含此內容
+            //// 如果父節點是漫畫名稱節點，表示此節點是回數(集數 )節點
+            //return IsComicNameNode(tn.Parent);
+        }
+
+        /// <summary>
+        /// 是否為顯示漫畫名稱的節點
+        /// </summary>
+        /// <param name="tn"></param>
+        /// <returns></returns>
+        public static bool IsComicNameNode(TreeNode tn)
+        {
+            return tn.Level == 2;
+            //if (null == tn) return false;
+            //if (false == tn.Name.Contains(XindmWebSite.WebUrl)) return false; // 節點本身必需要是包含此內容
+            //// 如果父節點是清單連結，表示此連結是漫畫名稱連結
+            //return IsListNode(tn.Parent);
+        }
+
+        /// <summary>
+        /// 是否為顯示漫畫清單的節點
+        /// </summary>
+        /// <param name="tn"></param>
+        /// <returns></returns>
+        public static bool IsListNode(TreeNode tn)
+        {
+            return tn.Level == 1;
+            //if (null == tn) return false;
+            //return tn.Name.Contains(XindmWebSite.ListUrl);
+        }
+
+    }
+}
---
+++ ComicCatcher/App_Code/Utils/TreeViewUtil.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Utils
+{
+    public static class TreeViewUtil
+    {
+        private static Font boldFont = new Font("新細明體", 10, FontStyle.Bold);
+        public static void SetFontBold(TreeNode tn)
+        {
+            if (tn.TreeView == null) return;
+            if (tn.TreeView.InvokeRequired)
+                tn.TreeView.Invoke(new MethodInvoker(() => tn.NodeFont = boldFont));
+            else
+                lock (tn)
+                {
+                    tn.NodeFont = boldFont;
+                }
+        }
+
+        public static void SetFontBold(TreeNode tn, Color color)
+        {
+            if (tn.TreeView == null) return;
+            if (tn.TreeView.InvokeRequired)
+                tn.TreeView.Invoke(new MethodInvoker(() => { tn.NodeFont = boldFont; tn.ForeColor = color; }));
+            else
+            {
+                lock (tn)
+                {
+                    tn.NodeFont = boldFont;
+                    tn.ForeColor = color;
+                }
+            }
+        }
+
+        public static void ClearTreeNode(TreeNode currNode)
+        {
+            if (currNode.TreeView == null) return;
+            if (currNode.TreeView.InvokeRequired)
+                currNode.TreeView.Invoke(new MethodInvoker(() => currNode.Nodes.Clear()));
+            else
+                lock (currNode)
+                {
+                    currNode.Nodes.Clear();
+                }
+        }
+
+        public static TreeNode AddTreeNode(TreeNode parentNode, TreeNode childNode)
+        {
+            if (parentNode.TreeView == null) return null;
+            if (parentNode.TreeView.InvokeRequired)
+                parentNode.TreeView.Invoke(new MethodInvoker(() => parentNode.Nodes.Add(childNode)));
+            else
+                lock (parentNode)
+                {
+                    parentNode.Nodes.Add(childNode);
+                }
+            return childNode;
+        }
+
+        public static TreeNode AddTreeNode(TreeNode parentNode, string key, string text)
+        {
+            TreeNode tn = new TreeNode();
+            if (parentNode.TreeView == null) return null;
+            if (parentNode.TreeView.InvokeRequired)
+                parentNode.TreeView.Invoke(new MethodInvoker(() => tn = parentNode.Nodes.Add(key, text)));
+            else
+                lock (parentNode)
+                {
+                    tn = parentNode.Nodes.Add(key, text);
+                }
+            return tn;
+        }
+
+        public static TreeNode BuildNode(ComicModels.ComicName comic, string localComicPath)
+        {
+            TreeNode nameNode = new TreeNode();
+            nameNode.Name = comic.Url;
+            nameNode.Text = comic.Caption;
+            nameNode.ImageKey = comic.IconUrl;
+            nameNode.Tag = comic;
+
+            // 如果本地端已有此漫畫的資料夾，改為粗體顯示
+            if (Directory.Exists(Path.Combine(localComicPath, nameNode.Text)))
+            {
+                nameNode.NodeFont = boldFont;
+            }
+
+            return nameNode;
+        }
+
+        public static TreeNode BuildNode(ComicModels.ComicChapter chapter, string webSiteName, string comicName)
+        {
+            TreeNode chapterNode = new TreeNode();
+            chapterNode.Name = chapter.Url;
+            chapterNode.Text = chapter.Caption;
+            chapterNode.Tag = chapter;
+
+            // 如果是已下載過的點，變粗體，改顏色
+            if (Models.DownloadedList.HasDownloaded(webSiteName, comicName, chapterNode.Text))
+            {
+                chapterNode.NodeFont = boldFont;
+                chapterNode.ForeColor = Color.Blue;
+            }
+
+            return chapterNode;
+        }
+    }
+}
+
