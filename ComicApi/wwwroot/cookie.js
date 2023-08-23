c6cb917e353923269bf30274b2b722ee40db6ea2
paul <paul.chen@winka.io>
2023-08-23 14:23:36 +0800

    add loading

---
+++ ComicApi/wwwroot/cookie.js
@@ -0,0 +1,29 @@
+﻿function addCookie(key, value) {
+    document.cookie = `${key}=${value};path=/;`;
+}
+
+function addLoginCookie() {
+    var newuserid = document.querySelector('#userid').value;
+    document.cookie = `userid=${newuserid};path=/;`;
+    location.reload();
+}
+function deleteLoginCookie() {
+    document.cookie = `userid=;path=/;`;
+    location.reload();
+}
+
+function getCookie(cookieKey) {
+    let cookieName = `${cookieKey}=`;
+    let cookieArray = document.cookie.split(';');
+
+    for (let cookie of cookieArray) {
+
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+
+        if (cookie.indexOf(cookieName) == 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+}
