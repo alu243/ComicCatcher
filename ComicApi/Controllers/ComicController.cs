2802dc159537a2817451b27b5b8ad1e3dbad87a0
paul <paul.chen@winka.com>
2024-07-07 05:55:01 +0800

    先用 proxy 方式解決 refferer 的問題

--- ComicApi/Controllers/ComicController.cs
+++ ComicApi/Controllers/ComicController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace ComicApi.Controllers
 {
@@ -6,10 +7,16 @@ namespace ComicApi.Controllers
     public class ComicController : Controller
     {
         private ComicApplication app;
+        private static  HttpClient _httpClient = null;
 
         public ComicController(ComicApplication comicApplication)
         {
             app = comicApplication;
+            if (_httpClient == null)
+            {
+                var handler = new SocketsHttpHandler() { UseCookies = true, Proxy = null };
+                _httpClient = new HttpClient(handler);// { BaseAddress = baseAddress };
+            }
         }
 
         [HttpGet("{comic}")]
@@ -23,6 +30,35 @@ namespace ComicApi.Controllers
         {
             var comicEnity = await app.GetComic(comic);
             return View("ShowPagesInChapter", comicEnity);
+        }
+
+
+        [HttpGet("{comic}/{chapter}/{url}")]
+        public async Task<IActionResult> GetImage(string comic, string chapter, string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return BadRequest("URL is required");
+            }
+
+            try
+            {
+                var requestMessage = new HttpRequestMessage(HttpMethod.Get, Uri.UnescapeDataString(url));
+                requestMessage.Headers.Referrer = new Uri($"https://www.dm5.cn/{chapter}/");
+
+                var response = await _httpClient.SendAsync(requestMessage);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return StatusCode((int)response.StatusCode);
+                }
+                var content = await response.Content.ReadAsByteArrayAsync();
+                return File(content, "image/jpeg"); // 你可以根据图片的 MIME 类型修改这里
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
